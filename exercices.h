b67908ecefa3f8b0fededed61f9d1f75e363f2c7
Valentin Kaelin <kalanehd@gmail.com>
2020-09-18 17:32:29 +0200

    fix: defbine name in header file

--- exercices.h
+++ exercices.h
@@ -2,11 +2,11 @@
 // Created by Valentin Kaelin on 18.09.20.
 //
 
-#ifndef PREMIERPROJET_EX1X2_H
-#define PREMIERPROJET_EX1X2_H
+#ifndef PREMIERPROJET_EXERCICES_H
+#define PREMIERPROJET_EXERCICES_H
 
 void ex1_2();
 void ex1_3();
 void ex1_4();
 
-#endif //PREMIERPROJET_EX1X2_H
+#endif //PREMIERPROJET_EXERCICES_H
