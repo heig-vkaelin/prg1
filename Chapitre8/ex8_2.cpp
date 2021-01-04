65d2efbf772e004510c6953469edf0f60e98ae54
Valentin Kaelin <kalanehd@gmail.com>
2021-01-04 14:45:59 +0100

    refactor(8.2): use << operator instead of custom toString function

--- Chapitre8/ex8_2.cpp
+++ Chapitre8/ex8_2.cpp
@@ -5,21 +5,24 @@
 #include <cstdlib>
 #include <iostream>
 
+using namespace std;
+
 class Temps {
 public:
-	Temps(const std::string &heures, const std::string &minutes)
+	Temps(const string &heures, const string &minutes)
 		: heures(heures), minutes(minutes) {}
 
 	bool operator==(const Temps &t) const {
 		return t.heures == heures && t.minutes == minutes;
 	}
 
-	std::string toString() const {
-		return heures + ":" + minutes;
+	friend ostream &operator<<(ostream &lhs, const Temps &rhs) {
+		lhs << rhs.heures + ":" + rhs.minutes;
+		return lhs;
 	}
 
 private:
-	std::string heures, minutes;
+	string heures, minutes;
 };
 
 template<typename T>
@@ -31,12 +34,10 @@ unsigned nbOcc(const T tab[], const T &element, size_t taille) {
 	return nbOcc;
 }
 
-using namespace std;
-
 int ex8_2() {
 	int tab1[] = {0, 1, 0};
 	int valAChercher = 0;
-	cout << "Nombre de valeur " + to_string(valAChercher) + ": "
+	cout << "Nombre de valeur " << valAChercher << ": "
 		  << nbOcc(tab1, valAChercher, 3) << endl;
 
 	Temps tab2[] = {
@@ -46,17 +47,17 @@ int ex8_2() {
 		Temps("21", "30"),
 	};
 	Temps tempsAChercher("12", "00");
-	cout << "Nombre de " + tempsAChercher.toString() + ": "
+	cout << "Nombre de " << tempsAChercher << ": "
 		  << nbOcc(tab2, tempsAChercher, 4) << endl;
 
 	string tab3[] = {"Paul", "Jacques", "Paul", "Jean", "Paul"};
 	string strAChercher = "Paul";
-	cout << "Nombre de \"" + strAChercher + "\": "
+	cout << "Nombre de \"" << strAChercher << "\": "
 		  << nbOcc(tab3, strAChercher, 5) << endl;
 
 //	const char *tab4[] = {"Paul", "Jacques", "Paul", "Jean", "Paul"};
 //	const char *charATrouver = "Paul";
-//	cout << "Nombre de \"" + strAChercher + "\": "
+//	cout << "Nombre de \"" << strAChercher << "\": "
 //		  << nbOcc(tab4, charATrouver, 5) << endl;
 
 	return EXIT_SUCCESS;
