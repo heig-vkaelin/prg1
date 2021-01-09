f0030182a746d23f9a2568e7b8a160999afd3775
Valentin Kaelin <kalanehd@gmail.com>
2021-01-09 22:37:56 +0100

    refactor(7.12): pisso de fluti gitkraken

--- Chapitre7/Ex7_12/hobby.h
+++ Chapitre7/Ex7_12/hobby.h
@@ -5,4 +5,11 @@
 #ifndef PRG1_EXERCICES_HOBBY_H
 #define PRG1_EXERCICES_HOBBY_H
 
-#endif //PRG1_EXERCICES_HOBBY_H
+#include <string>
+
+enum class Hobby {
+	SPORT, MUSIQUE, CINEMA, LECTURE
+};
+const std::string HOBBIES[] = {"sport", "musique", "cinema", "lecture"};
+
+#endif // PRG1_EXERCICES_HOBBY_H
